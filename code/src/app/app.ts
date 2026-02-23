8e9ba543932f489522382e888339ee4aa56ff979
Antonio040 <a.petrov@students.htl-leonding.ac.at>
2026-02-23 10:05:05 +0100

    basic form for login, doesn't work yet.

--- code/src/app/app.ts
+++ code/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
+import {Login} from './login/login';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: 'app-root',
+	templateUrl: './app.html',
+	imports: [
+		Login
+	],
+	styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('roomFood');
