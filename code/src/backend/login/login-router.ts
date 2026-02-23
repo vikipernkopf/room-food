60bc6b607807d99f2c221f35fd5af73c7fcd200a
vikipernkopf <v.pernkopf@students.htl-leonding.ac.at>
2026-02-23 20:33:17 +0100

    try to split up backend and frontend

--- code/src/backend/login/login-router.ts
+++ code/src/backend/login/login-router.ts
@@ -5,11 +5,12 @@ import { StatusCodes } from "http-status-codes";
 
 export const loginRouter = express.Router();
 
-loginRouter.get('/all', (_, res) => {
+loginRouter.get('/test', (_, res) => {
 	  const unit = new Unit();
-	  try
-	  const loginService = new LoginService(unit);
-	  const users = loginService.getAllUsers();
+
+	  const __ = new LoginService(unit);
+
 	  unit.complete();
-	  res.status(StatusCodes.OK).json(users);
+
+	  res.sendStatus(StatusCodes.NO_CONTENT);
 })
