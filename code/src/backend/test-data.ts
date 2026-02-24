855d0d79938fd16f74673321f1ce967a2a839b38
vikipernkopf <v.pernkopf@students.htl-leonding.ac.at>
2026-02-24 17:34:25 +0100

    update test script

--- code/src/backend/test-data.ts
+++ code/src/backend/test-data.ts
@@ -1,7 +1,7 @@
-import { LoginService} from './login/login-service';
+import { LoginSignUpService } from './login-sign-up/login-sign-up-service';
 import { Unit } from './unit';
 
 const unit = new Unit(false);
-const loginService = new LoginService(unit);
+const loginService = new LoginSignUpService(unit);
 console.log(loginService.addUser({username:"antonio", password:"1234"}));
 unit.complete(true);
