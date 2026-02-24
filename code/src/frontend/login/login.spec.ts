8c4284a9a570a3ac8e84a3ba4402e3afc9b4fb7f
vikipernkopf <v.pernkopf@students.htl-leonding.ac.at>
2026-02-24 13:51:55 +0100

    fix login tests

--- code/src/frontend/login/login.spec.ts
+++ code/src/frontend/login/login.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Login } from './login';
+import { provideRouter } from '@angular/router';
+import { provideLocationMocks } from '@angular/common/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('Login', () => {
   let component: Login;
@@ -8,7 +11,12 @@ describe('Login', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Login]
+      imports: [Login],
+      providers: [
+        provideHttpClientTesting(),
+        provideRouter([]),
+        provideLocationMocks()
+      ]
     })
     .compileComponents();
 
