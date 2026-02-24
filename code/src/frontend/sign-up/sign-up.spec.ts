b32edb92ec31fce0b77f87570e166f6ea719eed3
vikipernkopf <v.pernkopf@students.htl-leonding.ac.at>
2026-02-24 17:38:10 +0100

    update test scripts

--- code/src/frontend/sign-up/sign-up.spec.ts
+++ code/src/frontend/sign-up/sign-up.spec.ts
@@ -1,23 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignUp } from './sign-up';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
+import {provideRouter} from '@angular/router';
+import {provideLocationMocks} from '@angular/common/testing';
 
-describe('Signup', () => {
-  let component: SignUp;
-  let fixture: ComponentFixture<SignUp>;
+describe('SignUp', () => {
+  	let component: SignUp;
+  	let fixture: ComponentFixture<SignUp>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SignUp]
-    })
-    .compileComponents();
+  	beforeEach(async () => {
+    	await TestBed.configureTestingModule({
+			imports: [SignUp],
+			providers: [
+				provideHttpClientTesting(),
+				provideRouter([]),
+				provideLocationMocks()
+			]
+    	}).compileComponents();
 
-    fixture = TestBed.createComponent(SignUp);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+    	fixture = TestBed.createComponent(SignUp);
+    	component = fixture.componentInstance;
+    	await fixture.whenStable();
+  	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  	it('should create', () => {
+    	expect(component).toBeTruthy();
+  	});
 });
