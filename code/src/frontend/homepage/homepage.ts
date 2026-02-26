c2abf520231f6c4b395d9a3e23c946e9af6cabdf
Antonio040 <a.petrov@students.htl-leonding.ac.at>
2026-02-26 20:44:38 +0100

    Navbar added + some functionality to the navbar + css + idk

--- code/src/frontend/homepage/homepage.ts
+++ code/src/frontend/homepage/homepage.ts
@@ -1,9 +1,12 @@
 import {Component, signal, WritableSignal} from '@angular/core';
 import {AuthService} from '../core/auth-service';
+import {Navbar} from '../navbar/navbar';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+	imports: [
+		Navbar
+	],
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss',
 })
