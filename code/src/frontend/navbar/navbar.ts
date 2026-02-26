9156ef4cf6e9e7a2ec57e73e4fad16b6b92a86cf
vikipernkopf <v.pernkopf@students.htl-leonding.ac.at>
2026-02-26 23:46:56 +0100

    update vite config, navbar routing and css of various pages

--- code/src/frontend/navbar/navbar.ts
+++ code/src/frontend/navbar/navbar.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+	imports: [
+		NgOptimizedImage,
+		RouterLink,
+		RouterLinkActive
+	],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
