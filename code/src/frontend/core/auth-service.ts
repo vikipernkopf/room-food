acc6ed1a0d342ed9ab93b9d02b4218dc4a6f2231
Antonio040 <a.petrov@students.htl-leonding.ac.at>
2026-03-20 11:20:31 +0100

    delete profile working

--- code/src/frontend/core/auth-service.ts
+++ code/src/frontend/core/auth-service.ts
@@ -45,7 +45,14 @@ export class AuthService {
 			}
 		});
 	}
-
+	deleteUser(credentials: LoginCredentials): Observable<User> {
+		return this.http.delete<User>(`${this.apiBase}/delete`, { body: credentials }).pipe(
+			tap((user) => {
+				console.log(`User ${user.username} deleted successfully`);
+				this.logout();
+			})
+		);
+	}
 	signUp(credentials: SignUpCredentials) {
 		this.http.post<User>(`${this.apiBase}/signup`, credentials).subscribe({
 			next: (user) => {
